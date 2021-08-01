5a68f366587491a9e39bcb4958e9c9f3dadb9b87
seangnewman <seangnewman@msn.com>
2021-08-01 02:05:52 -0700

    Add project files.

---
+++ BethanysPieShopHRM.ComponentsLibrary/ProfilePictureBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.ComponentsLibrary
+{
+    public class ProfilePictureBase : ComponentBase
+    {
+        protected string CssClass { get; set; } = "circle";
+
+        protected void PictureClick()
+        {
+            CssClass = CssClass == "circle" ? null : "circle";
+        }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
